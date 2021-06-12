22b9bd40a3ffc64d6b4027a68e823c63b425aa55
kamaal111 <kamaal.f1@gmail.com>
2021-06-12 18:22:37 +0200

    Finishing off script

--- PhotogrammetryMaker/main.swift
+++ PhotogrammetryMaker/main.swift
@@ -11,86 +11,122 @@ import Combine
 
 struct PhotogrammetryMaker {
 
-    private init() { }
+    init() { }
 
-    static func main() {
-        guard let rootURL = URL.projectRoot else { return }
-        let imagesDirectoryURL = rootURL.appendingPathComponent("Rock36Images")
+    func run() {
+        guard let rootURL = URL.projectRoot else {
+            print(Errors.rootURLNotFound)
+            Foundation.exit(1)
+        }
+        let photogrammetrySessionResult = makePhotogrammetrySession(rootURL: rootURL)
+        let photogrammetrySession: PhotogrammetrySession
+        switch photogrammetrySessionResult {
+        case .failure(let failure):
+            print(failure)
+            Foundation.exit(1)
+        case .success(let success): photogrammetrySession = success
+        }
+        var subscriptions = Set<AnyCancellable>()
+        photogrammetrySession.output
+            .receive(on: DispatchQueue.global())
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let failure):
+                    print(failure)
+                    Foundation.exit(1)
+                case .finished: break
+                }
+            }, receiveValue: { output in
+                switch output {
+                case .processingComplete: print("processingComplete")
+                case .requestComplete(let request, let result):
+                    print(request)
+                    print(result)
+                    print("requestComplete")
+                case .inputComplete:
+                    print("inputComplete")
+                case .requestError(let request, let error):
+                    print(request)
+                    print(error)
+                    print("requestError")
+                case .requestProgress(let request, fractionComplete: let fractionComplete):
+                    print(request)
+                    print(fractionComplete)
+                    print("requestProgress")
+                case .processingCancelled:
+                    print("processingCancelled")
+                case .invalidSample(id: let id, reason: let reason):
+                    print(id)
+                    print(reason)
+                    print("invalidSample")
+                case .skippedSample(id: let id):
+                    print(id)
+                    print("skippedSample")
+                case .automaticDownsampling:
+                    print("automaticDownsampling")
+                @unknown default:
+                    print("unknown case")
+                    print(output)
+                }
+            })
+            .store(in: &subscriptions)
+        let outPutURL = rootURL.appendingPathComponent("NewObject.usdz")
+        if let maybeError = processPhotogrammetrySession(session: photogrammetrySession, outPutURL: outPutURL) {
+            print(maybeError)
+            Foundation.exit(1)
+        }
+    }
+
+    private func processPhotogrammetrySession(session: PhotogrammetrySession, outPutURL: URL) -> Errors? {
+        let request = PhotogrammetrySession.Request.modelFile(url: outPutURL, detail: .full)
+        do {
+            try session.process(requests: [request])
+        } catch {
+            return .general(error: error)
+        }
+        return nil
+    }
+
+    private func makePhotogrammetrySession(rootURL: URL) -> Result<PhotogrammetrySession, Errors> {
+        let imagesDirectoryURLResult = getImagesDirectoryURL(rootURL: rootURL)
+        let imagesDirectoryURL: URL
+        switch imagesDirectoryURLResult {
+        case .failure(let failure): return .failure(failure)
+        case .success(let success): imagesDirectoryURL = success
+        }
+        let sessionConfiguration = makePhotogrammetrySessionConfiguration()
+        let session: PhotogrammetrySession
+        do {
+            session = try PhotogrammetrySession(input: imagesDirectoryURL.fileURLPath, configuration: sessionConfiguration)
+        } catch {
+            return .failure(.general(error: error))
+        }
+        return .success(session)
+    }
+
+    private func makePhotogrammetrySessionConfiguration() -> PhotogrammetrySession.Configuration {
+        let sessionConfiguration = PhotogrammetrySession.Configuration()
+        return sessionConfiguration
+    }
+
+    private func getImagesDirectoryURL(rootURL: URL) -> Result<URL, Errors> {
+        let imagesDirectoryURL = rootURL.appendingPathComponent("Rock36Images", isDirectory: true)
         let contentsOfImagesDirectory: [URL]
         do {
             contentsOfImagesDirectory = try FileManager.default.contentsOfDirectory(at: imagesDirectoryURL, includingPropertiesForKeys: nil, options: [])
         } catch {
-            print(error)
-            Foundation.exit(1)
+            return .failure(.general(error: error))
         }
         guard !contentsOfImagesDirectory.isEmpty else {
-            print("no images found in directory")
-            Foundation.exit(1)
+            return .failure(.emptyDirectory)
         }
-        let outPutURL = rootURL.appendingPathComponent("NewObject.usdz")
-        let request = PhotogrammetrySession.Request.modelFile(url: outPutURL,
-                                                              detail: .full)
-//        let sessionConfiguration = PhotogrammetrySession.Configuration()
-//        let session: PhotogrammetrySession
-        do {
-            let session = try PhotogrammetrySession(input: imagesDirectoryURL.fileURLPath)
-            print(session)
-        } catch {
-            print("session")
-            print(error)
-            Foundation.exit(1)
-        }
-//        var subscriptions = Set<AnyCancellable>()
-//        session.output
-//            .receive(on: DispatchQueue.global())
-//            .sink(receiveCompletion: { completion in
-//                switch completion {
-//                case .failure(let failure):
-//                    print(failure)
-//                    Foundation.exit(1)
-//                case .finished: break
-//                }
-//            }, receiveValue: { output in
-//                switch output {
-//                case .processingComplete: print("processingComplete")
-//                case .requestComplete(let request, let result):
-//                    print(request)
-//                    print(result)
-//                    print("requestComplete")
-//                case .inputComplete:
-//                    print("inputComplete")
-//                case .requestError(let request, let error):
-//                    print(request)
-//                    print(error)
-//                    print("requestError")
-//                case .requestProgress(let request, fractionComplete: let fractionComplete):
-//                    print(request)
-//                    print(fractionComplete)
-//                    print("requestProgress")
-//                case .processingCancelled:
-//                    print("processingCancelled")
-//                case .invalidSample(id: let id, reason: let reason):
-//                    print(id)
-//                    print(reason)
-//                    print("invalidSample")
-//                case .skippedSample(id: let id):
-//                    print(id)
-//                    print("skippedSample")
-//                case .automaticDownsampling:
-//                    print("automaticDownsampling")
-//                @unknown default:
-//                    print("unknown case")
-//                    print(output)
-//                }
-//            })
-//            .store(in: &subscriptions)
-//
-//        do {
-//            try session.process(requests: [request])
-//        } catch {
-//            print(error)
-//            Foundation.exit(1)
-//        }
+        return .success(imagesDirectoryURL)
+    }
+
+    private enum Errors: Error {
+        case rootURLNotFound
+        case general(error: Error)
+        case emptyDirectory
     }
 
 }
@@ -107,4 +143,4 @@ extension URL {
 
 // MARK: - Main
 
-PhotogrammetryMaker.main()
+PhotogrammetryMaker().run()
